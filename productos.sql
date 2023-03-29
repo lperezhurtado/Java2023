6b10cd8bc67f0ae7bfc63c47dc8c2f5ead0dbb9b
Luis Perez Hurtado <lperezhurtado97@gmail.com>
2023-03-29 10:56:50 +0200

    prueba de am

--- productos.sql
+++ productos.sql
@@ -24,9 +24,7 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `productos`
---
+
 
 CREATE TABLE `productos` (
   `CODIGO_ARTICULO` varchar(4) NOT NULL,
